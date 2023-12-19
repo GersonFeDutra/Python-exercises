9a13e7573472d9c06192c5fb327e11844a865303
GersonFeDutra <62914156+GersonFeDutra@users.noreply.github.com>
2023-12-19 20:01:37 -0300

    Update servidor_hora.py

--- Sistemas Distribuídos/socket/udp/servidor_hora.py
+++ Sistemas Distribuídos/socket/udp/servidor_hora.py
@@ -1,1 +1,48 @@
 
+"""
+1) Escreva um programa em Python, modelo cliente/servidor, com sockets TCP.
+- O servidor deve atender um único pedido do cliente: Retornar o horário do momento solicitado.
+- O cliente deve solicitar o horário ao servidor e encerrar. 
+- O servidor deve ser multithread. 
+2) Faça o mesmo para sockets UDP.
+
+Utilize como base:
+<https://github.com/rafaelperazzo/cc0026/blob/master/sockets/servidor_multithread.py>
+<https://github.com/rafaelperazzo/cc0026/blob/master/sockets/cliente_multithread.py>
+"""
+import socket
+import time
+
+HOST: str = 'localhost'  # Standard loopback interface address (127.0.0.1)
+PORT: int = 8082  # Port to listen on (non-privieged ports are > 1023)
+DATA_PAYLOAD: int = 1024  # The maximum amount of data to be received at once
+
+
+def main(*_args) -> None:
+    # Create a TCP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Bind the socket to the port
+    SERVER_ADDRESS = (HOST, PORT)
+    print('\033[34m''Starting up echo server on %s port %s''\033[m' %
+        SERVER_ADDRESS)
+
+    sock.bind(SERVER_ADDRESS)
+
+    while True:
+        data, address = sock.recvfrom(DATA_PAYLOAD)
+        print(f'Received message from client {address}')
+        message = data.decode()
+
+        if message == '-t':
+            print(f'Sending current time... to {address}')
+
+            new_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            message = str(time.strftime("%H:%M:%S", time.localtime())).encode()
+            new_sock.sendto(message, address)
+        else:
+            print('Error: Invalid request')
+
+
+if __name__ == '__main__':
+    main()
