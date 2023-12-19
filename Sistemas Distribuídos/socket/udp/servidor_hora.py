fa7c397de9e02939dc12f2c2ed53813289d1fdbd
GersonFeDutra <62914156+GersonFeDutra@users.noreply.github.com>
2023-12-19 20:34:21 -0300

    Update servidor_hora.py

--- Sistemas Distribuídos/socket/udp/servidor_hora.py
+++ Sistemas Distribuídos/socket/udp/servidor_hora.py
@@ -1,4 +1,3 @@
-
 """
 1) Escreva um programa em Python, modelo cliente/servidor, com sockets TCP.
 - O servidor deve atender um único pedido do cliente: Retornar o horário do momento solicitado.
@@ -12,11 +11,25 @@ Utilize como base:
 """
 import socket
 import time
+from threading import Thread
 
 HOST: str = 'localhost'  # Standard loopback interface address (127.0.0.1)
 PORT: int = 8082  # Port to listen on (non-privieged ports are > 1023)
 DATA_PAYLOAD: int = 1024  # The maximum amount of data to be received at once
 
+def res(data: bytes, address):
+    
+    print(f'Received message from client {address}')
+    message = data.decode()
+
+    if message == '-t':
+        print(f'Sending current time... to {address}')
+
+        new_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        message = str(time.strftime("%H:%M:%S", time.localtime())).encode()
+        new_sock.sendto(message, address)
+    else:
+        print('Error: Invalid request')
 
 def main(*_args) -> None:
     # Create a TCP socket
@@ -30,18 +43,9 @@ def main(*_args) -> None:
     sock.bind(SERVER_ADDRESS)
 
     while True:
-        data, address = sock.recvfrom(DATA_PAYLOAD)
-        print(f'Received message from client {address}')
-        message = data.decode()
-
-        if message == '-t':
-            print(f'Sending current time... to {address}')
-
-            new_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            message = str(time.strftime("%H:%M:%S", time.localtime())).encode()
-            new_sock.sendto(message, address)
-        else:
-            print('Error: Invalid request')
+        pei = sock.recvfrom(DATA_PAYLOAD)
+        Thread(target=res, args=pei)\
+            .start()
 
 
 if __name__ == '__main__':
