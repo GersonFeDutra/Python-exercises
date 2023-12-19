40b0315969a2c66b05e5c94a6fc4374dc7194f69
GersonFeDutra <62914156+GersonFeDutra@users.noreply.github.com>
2023-12-19 19:26:22 -0300

    Update cliente_hora.py

--- Sistemas Distribuídos/socket/tcp/cliente_hora.py
+++ Sistemas Distribuídos/socket/tcp/cliente_hora.py
@@ -1,1 +1,28 @@
+import socket
 
+HOST: str = 'localhost'
+PORT: int = 8081
+
+
+def main(*_args) -> None:
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    CLIENT_ADDRESS = (HOST, PORT)
+    print('Connection to %s port %s' % CLIENT_ADDRESS)
+
+    # Connect the socket to the server
+    sock.connect(CLIENT_ADDRESS)
+
+    sock.sendall('-t'.encode())
+
+    DATA_PAYLOAD: int = 1024
+    data = sock.recv(DATA_PAYLOAD)
+    print(f'Time received: {data.decode()}')
+
+    print('Closing connection to the server')
+    sock.close()
+
+
+if __name__ == '__main__':
+    main()
