2d1cd43ab04f6267b3aeb194916ce8c2e6ce9a65
GersonFeDutra <62914156+GersonFeDutra@users.noreply.github.com>
2023-12-19 19:27:05 -0300

    Update servidor_hora.py

--- Sistemas Distribuídos/socket/tcp/servidor_hora.py
+++ Sistemas Distribuídos/socket/tcp/servidor_hora.py
@@ -9,3 +9,42 @@ Utilize como base:
 <https://github.com/rafaelperazzo/cc0026/blob/master/sockets/servidor_multithread.py>
 <https://github.com/rafaelperazzo/cc0026/blob/master/sockets/cliente_multithread.py>
 """
+import socket
+import time
+
+HOST: str = 'localhost'  # Standard loopback interface address (127.0.0.1)
+PORT: int = 8081  # Port to listen on (non-privieged ports are > 1023)
+DATA_PAYLOAD: int = 1024  # The maximum amount of data to be received at once
+
+
+def main(*_args) -> None:
+    # Create a TCP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the port
+    SERVER_ADDRESS = (HOST, PORT)
+    print('\033[34m''Starting up echo server on %s port %s''\033[m' %
+        SERVER_ADDRESS)
+
+    sock.bind(SERVER_ADDRESS)
+
+    MAX_QUEUE_CONNECT: int = 1  # specifies the max no, of queued connections
+    sock.listen(MAX_QUEUE_CONNECT)  # Listen to clients
+    client, address = sock.accept()
+
+    print('Waiting to receive a message from client...')
+    data = client.recv(DATA_PAYLOAD)
+    message = data.decode()
+
+    if message == '-t':
+        print(f'Sending current time... to {address}')
+        client.sendall(f'{time.strftime("%H:%M:%S", time.localtime())}'.encode())
+    else:
+        print('Error: Invalid request')
+
+    print('Closing client connection', address)
+    client.close()
+
+
+if __name__ == '__main__':
+    main()
