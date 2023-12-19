a26a87e8f038880aba2d4f74239af3825c99e21b
GersonFeDutra <62914156+GersonFeDutra@users.noreply.github.com>
2023-12-19 18:27:47 -0300

    Update and rename server.py to servidor_hora.py

---
+++ Sistemas Distribuídos/socket/servidor_hora.py
@@ -0,0 +1,11 @@
+"""
+1) Escreva um programa em Python, modelo cliente/servidor, com sockets TCP.
+- O servidor deve atender um único pedido do cliente: Retornar o horário do momento solicitado.
+- O cliente deve solicitar o horário ao servidor e encerrar. 
+- O servidor deve ser multithread. 
+2) Faça o mesmo para sockets UDP.
+
+Utilize como base:
+<https://github.com/rafaelperazzo/cc0026/blob/master/sockets/servidor_multithread.py>
+<https://github.com/rafaelperazzo/cc0026/blob/master/sockets/cliente_multithread.py>
+"""
