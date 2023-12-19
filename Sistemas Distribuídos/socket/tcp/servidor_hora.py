77a67718f4633f9435319d85b4e08fc59b02244c
GersonFeDutra <62914156+GersonFeDutra@users.noreply.github.com>
2023-12-19 20:33:15 -0300

    Update servidor_hora.py

--- Sistemas Distribuídos/socket/tcp/servidor_hora.py
+++ Sistemas Distribuídos/socket/tcp/servidor_hora.py
@@ -11,27 +11,13 @@ Utilize como base:
 """
 import socket
 import time
+from threading import Thread
 
 HOST: str = 'localhost'  # Standard loopback interface address (127.0.0.1)
-PORT: int = 8081  # Port to listen on (non-privieged ports are > 1023)
+PORT: int = 8083  # Port to listen on (non-privieged ports are > 1023)
 DATA_PAYLOAD: int = 1024  # The maximum amount of data to be received at once
 
-
-def main(*_args) -> None:
-    # Create a TCP socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Bind the socket to the port
-    SERVER_ADDRESS = (HOST, PORT)
-    print('\033[34m''Starting up echo server on %s port %s''\033[m' %
-        SERVER_ADDRESS)
-
-    sock.bind(SERVER_ADDRESS)
-
-    MAX_QUEUE_CONNECT: int = 1  # specifies the max no, of queued connections
-    sock.listen(MAX_QUEUE_CONNECT)  # Listen to clients
-    client, address = sock.accept()
-
+def response(client: socket.socket, address):
     print('Waiting to receive a message from client...')
     data = client.recv(DATA_PAYLOAD)
     message = data.decode()
@@ -44,6 +30,25 @@ def main(*_args) -> None:
 
     print('Closing client connection', address)
     client.close()
+
+def main(*_args) -> None:
+    # Create a TCP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Bind the socket to the port
+    SERVER_ADDRESS = (HOST, PORT)
+    print('\033[34m''Starting up echo server on %s port %s''\033[m' %
+        SERVER_ADDRESS)
+
+    sock.bind(SERVER_ADDRESS)
+
+    MAX_QUEUE_CONNECT: int = 10  # specifies the max no, of queued connections
+    sock.listen(MAX_QUEUE_CONNECT)  # Listen to clients
+
+    while True:
+        client, address = sock.accept()
+        thread = Thread(target=response, args=(client, address))\
+                    .start()
 
 
 if __name__ == '__main__':
