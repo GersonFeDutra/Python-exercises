ce606e2084aff8657690094a79d22af775a44469
GersonFeDutra <62914156+GersonFeDutra@users.noreply.github.com>
2023-12-19 20:01:55 -0300

    Update cliente_hora.py

--- Sistemas Distribuídos/socket/udp/cliente_hora.py
+++ Sistemas Distribuídos/socket/udp/cliente_hora.py
@@ -1,1 +1,22 @@
+import socket
 
+HOST: str = 'localhost'
+PORT: int = 8082
+DATA_PAYLOAD: int = 1024
+
+def main(*_args) -> None:
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    print(f'Sending message to {HOST} port {PORT}')
+
+    sock.sendto('-t'.encode(), (HOST, PORT))
+
+    while True:
+        data, address = sock.recvfrom(DATA_PAYLOAD)
+        print(f'Time received: {data.decode()}')
+        break
+
+
+if __name__ == '__main__':
+    main()
